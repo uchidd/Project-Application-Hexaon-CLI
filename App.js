8a69d8b25c7180cbaf2ffd5cc124e6ce0c16a1aa
uchidd <mmi090903@gmail.com>
2020-02-11 11:59:18 +0700

    11:59, 11/02/20

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React, {Component} from "react";
 import { View, Text } from "react-native";
+import Header from "./headers/header";
+import { MenuProvider } from "react-native-popup-menu";
+import HeaderSearch from "./headers/headerSearch";
+import HeaderSearchWithButton from "./headers/headerSearchWithButton";
+import DrawerNavigator from "./navigation/DrawerNavigator";
 
 export default class App extends Component{
 
   render(){
     return(
-      <View>
-        <Text>Hello Tayo!</Text>
-      </View>
+      <MenuProvider>
+        <DrawerNavigator/>
+      </MenuProvider>
     )
   }
 }
